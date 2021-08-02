1028aa38b7b86190b967c19adb35c07db7168af6
Valentin Berger <Valentin.berger38@gmail.com>
2021-08-02 19:44:05 +0200

    :bookmark: Fix to version `0.0.2`

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@
 const String appName = "Tide";
 
 /// The app version.
-const String appVersion = "0.0.1+0";
+const String appVersion = "0.0.2";
 
 /// The app Git repo URL
 const gitRepoURL = "http://github.com/Cynnexis/tide";
