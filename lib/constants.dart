857d4c463c1355a150ec3dd65fdaeb613d4cba1e
Valentin Berger <Valentin.berger38@gmail.com>
2022-04-16 20:49:22 +0200

    :bookmark: Bump app version

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@
 const String appName = "Tide";
 
 /// The app version.
-const String appVersion = "0.0.2";
+const String appVersion = "1.0.0";
 
 /// The app Git repo URL
 const String gitRepoURL = "http://github.com/Cynnexis/tide";
